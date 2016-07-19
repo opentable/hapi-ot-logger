cfe9a38afe73c327c4ba3eefd51d4401516440e1
Arnold Zokas <arnold.zokas@coderoom.net>
2016-07-19 19:34:15 +0100

    tests for diagnostic logging

---
+++ tests/logger (diagnostic logging).js
@@ -0,0 +1,58 @@
+'use strict';
+
+var expect = require('expect.js');
+var joi    = require('joi');
+var rewire = require('rewire');
+var schema = require('./schema');
+
+describe('logger (diagnostic logging)', function() {
+    describe('when logging diagnostic info', function() {
+        var cfg
+        var consoleEntry;
+        var logEntry;
+
+        before(function(done) {
+            var logger = rewire('../lib/logger');
+
+            logger.config({
+                console: true,
+                payload: true
+            }, done);
+
+            cfg = logger.__get__("cfg");
+
+            logger.__set__('consoleWriter', {
+                log: function(_consoleEntry) {
+                    consoleEntry = _consoleEntry;
+                }
+            });
+
+            logger.__set__('client', {
+                rpush: function(listname, _logEntry) {
+                    logEntry = _logEntry;
+                }
+            });
+
+            logger.log({
+                data: {},
+                tags: ['tag']
+            });
+        });
+
+        it('should push diagnostic log to redis', function() {
+            joi.validate(logEntry, schema.log, function(err) {
+                if(err) {
+                    throw err;
+                }
+            });
+        });
+
+        it('should write to console out', function() {
+            expect(consoleEntry).to.not.be(undefined);
+        });
+
+        it('should increment sequence number', function() {
+            expect(cfg.sequencenumber).to.equal(2);
+        });
+    });
+});
